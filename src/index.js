1ac8e147aa7aee6db78c2d1d72b55d2cfcc302c7
AnushkaNemishte <anushkanemishte36@gmail.com>
2023-05-31 20:44:14 +0530

    Form

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "../node_modules/react-bootstrap/dist/react-bootstrap";
+/*
+  react bootstrap configuration
+*/
+
+import "../node_modules/react-bootstrap/dist/react-bootstrap"
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
