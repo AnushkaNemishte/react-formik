6e4afc7513d7bca81c2748b42cfd650c609cf16c
AnushkaNemishte <anushkanemishte36@gmail.com>
2023-06-01 22:37:43 +0530

    UseFormik

--- src/Components/YoutubeForm.js
+++ src/Components/YoutubeForm.js
@@ -1,30 +1,58 @@
 import React from 'react';
-import { Form } from "react-bootstrap"
+import { Button, Form } from "react-bootstrap"
+import { useFormik } from "formik"
+
 function YoutubeForm() {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      channel: ""
+    },
+    onSubmit:(values)=>{
+      console.log("Form data",values)
+    }
+  })
+ 
   return (
     <div className='container'>
-      <div className='form-header'>
-        <h1>YouTube Registration</h1>
-        <p>YouTube Registration Form !!!</p>
-        <div className='form-body'>
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>  Name </Form.Label>
-              <Form.Control type="text" placeholder="Enter Name" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>  Email Address </Form.Label>
-              <Form.Control type="email" placeholder="Enter Email" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>  Channel </Form.Label>
-              <Form.Control type="text" placeholder="Enter Channel " />
-            </Form.Group>
-          </Form>
-        </div>
-      </div>
+      <h1>Youtube Form</h1>
+      <Form onSubmit={formik.handleSubmit}>
+        <Form.Group className="mb-3" >
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text"
+            placeholder="Enter the name "
+            name='name'
+            id='name'
+            value={formik.values.name}
+            onChange={formik.handleChange}
 
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" >
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email"
+            placeholder="name@example.com"
+            name='email'
+            id='email'
+            value={formik.values.email}
+            onChange={formik.handleChange}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" >
+          <Form.Label>Channel Name</Form.Label>
+          <Form.Control type="text"
+            placeholder="Enter your youtube channel name"
+            name='channel'
+            id='channel'
+            value={formik.values.channel}
+            onChange={formik.handleChange}
+          />
+        </Form.Group>
+        <Button type='submit' variant='primary'>Submit</Button>
+      </Form>
     </div>
+
   );
 }
 
