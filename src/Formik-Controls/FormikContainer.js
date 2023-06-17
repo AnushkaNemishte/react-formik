6e9929784858c55f5c193fd2e077c5bb09c4b338
Anushka <anushka.nemishte@chitaledairy.com>
2023-06-17 15:43:55 +0530

    Input form control

--- src/Formik-Controls/FormikContainer.js
+++ src/Formik-Controls/FormikContainer.js
@@ -1,35 +1,52 @@
-import { Form, Formik } from 'formik'
-import React from 'react'
-import { Button } from 'react-bootstrap';
-import * as Yup from 'yup';
+import { Form, Formik } from "formik";
+import React from "react";
+import { Button } from "react-bootstrap";
+import * as Yup from "yup";
+import FormikControls from "./FormikControls";
 function FormikContainer() {
-    const initialValue={}
-    const onSubmit = (values)=>{
-        console.log("Form data ",values)
-    }
-    const validationSchema=Yup.object({
-
-    })
+  const initialValue = {
+    name: "",
+    email: "",
+  };
+  const onSubmit = (values) => {
+    console.log("Form data ", values);
+  };
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Required"),
+    email: Yup.string().email("Invalid Format").required("Required"),
+  });
   return (
-    <Formik 
-    initialValues={initialValue}
-    onSubmit={onSubmit}
-    validationSchema={validationSchema}
+    <Formik
+      initialValues={initialValue}
+      onSubmit={onSubmit}
+      validationSchema={validationSchema}
     >
-    {
-        (formikprops)=>{
-            return(
-            <div className='container'>
+      {(formikprops) => {
+        return (
+          <div className="container">
             <h1>Form Controls</h1>
             <Form>
-                <Button variant='primary' type='submit'>Submit</Button>
+              <FormikControls
+                control="input"
+                type="text"
+                label="Name"
+                name="name"
+              />
+               <FormikControls
+                control="input"
+                type="email"
+                label="Email"
+                name="email"
+              />
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
             </Form>
-            </div>
-            )
-        }
-    }
+          </div>
+        );
+      }}
     </Formik>
-  )
+  );
 }
 
-export default FormikContainer
+export default FormikContainer;
